607afd0afbc07c6ca2afaf017de52cf87ea1be47
marutamachi-dev <nkinoue6@gmail.com>
2026-05-12 00:46:17 +0900

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -51,6 +51,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
 
+    {
+      url: `${BASE_URL}/about-poikatsu`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
     ...offerUrls,
   ];
 }
