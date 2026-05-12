bf2033ce1de8cc61b52a23161071fe1b130c00a6
marutamachi-dev <nkinoue6@gmail.com>
2026-05-13 02:08:00 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
